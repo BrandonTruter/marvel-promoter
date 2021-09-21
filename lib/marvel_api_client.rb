5fcac3970207650987067c6a1b4c42051b042882
Brandon Lee Truter <brandon.truter@tauspace.com>
2021-09-21 15:00:52 +0200

    integrated with marvel API through first successful test case

--- lib/marvel_api_client.rb
+++ lib/marvel_api_client.rb
@@ -1,12 +1,22 @@
+require 'digest/md5'
 require "faraday"
 require "json"
 
 class MarvelApiClient
- def self.characters()
-   url = "https://gateway.marvel.com/v1/public/characters"
-   data = Faraday.get(url).body
-   JSON.parse(data, symbolize_names: true)
-   # create account for API key then use in request:
-   # response = Faraday.get(url, {apikey: ''})
- end
+  def self.set_md5(ts)
+    md5 = Digest::MD5.new
+    pub_key = "5f3ca374aec85caae447eff6ff94fed3"
+    pvt_key = "f10bc178862ee730c312491f165d4cc36d22d77c" # MOVE THIS TO CONFIG & GITIGNORE
+    _md5 = md5.update "#{ts}#{pvt_key}#{pub_key}"
+    _md5.hexdigest
+  end
+
+  def self.characters()
+    timestamp = Time.now
+    api_hash = set_md5(timestamp)
+    api_key = "5f3ca374aec85caae447eff6ff94fed3"
+    url = "https://gateway.marvel.com/v1/public/characters"
+    response = Faraday.get(url, {apikey: api_key, ts: timestamp, hash: api_hash})
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/requests/marvel_api_request_spec.rb
+++ spec/requests/marvel_api_request_spec.rb
@@ -2,18 +2,21 @@ require 'rails_helper'
 require 'marvel_api_client'
 
 RSpec.describe "MarvelApiClient", type: :request do
-
   describe 'GET /v1/public/characters' do
-    let(:characters_response) { MarvelApiClient.characters() }
+    let(:characters) { MarvelApiClient.characters() }
 
     it "returns a successful response" do
-      expect(characters_response.status).to eq(200)
+      expect(characters[:code]).to eq(200)
+      expect(characters[:status]).to eq("Ok")
     end
 
-    it "returns a specific character id" do
-      expect(characters_response).to be_kind_of(Hash)
-      expect(characters_response).to have_key(:id)
+    it "returns the attributionText" do
+      expect(characters).to have_key(:attributionText)
+    end
+
+    it "returns a character id" do
+      expect(characters).to be_kind_of(Hash)
+      expect(characters[:data][:results][0]).to have_key(:id)
     end
   end
-
 end
