72665c8e1ec6573f255b77b03a0ad208f1d8767e
Brandon Lee Truter <brandon.truter@tauspace.com>
2021-09-24 12:43:09 +0200

    added support for character and series image variants

--- app/controllers/marvel_controller.rb
+++ app/controllers/marvel_controller.rb
@@ -7,15 +7,47 @@ class MarvelController < ApplicationController
     @series = CharacterPromoter.new(marvel_series).as_series
     marvel = CharacterPromoter.new(character).as_marvel
     hero = CharacterPromoter.new(character).as_hero
-    @image = {
-      "landscape" => hero[:image_landscape],
-      "standard" => hero[:image_standard],
-      "portrait" => hero[:image_portrait]
-    }
     @attribute = marvel[:attribute]
     @description = hero[:description]
     @image_filename = hero[:image]
     @name = hero[:name]
+    images = hero[:images]
+    @image = thumbnail_helper(images)
+    # @image = {
+    #   "landscape" => hero[:image_landscape],
+    #   "standard" => hero[:image_standard],
+    #   "portrait" => hero[:image_portrait]
+    # }
+  end
+
+  def thumbnail_helper(images)
+    standard_images = images[:standard]
+    portrait_images = images[:portrait]
+    landscape_images = images[:landscape]
+    {
+      "standard" => {
+        "small" => standard_images[:small],
+        "medium" => standard_images[:medium],
+        "large" => standard_images[:large],
+        "amazing" => standard_images[:amazing],
+        "fantastic" => standard_images[:fantastic]
+      },
+      "portrait" => {
+        "small" => portrait_images[:small],
+        "medium" => portrait_images[:medium],
+        "large" => portrait_images[:large],
+        "uncanny" => portrait_images[:uncanny],
+        "amazing" => portrait_images[:amazing],
+        "fantastic" => portrait_images[:fantastic]
+      },
+      "landscape" => {
+        "small" => landscape_images[:small],
+        "medium" => landscape_images[:medium],
+        "large" => landscape_images[:large],
+        "amazing" => landscape_images[:amazing],
+        "incredible" => landscape_images[:incredible]
+      }
+    }
   end
 
   private
--- app/promoters/character_promoter.rb
+++ app/promoters/character_promoter.rb
@@ -12,21 +12,64 @@ class CharacterPromoter
    }
  end
  def as_hero
+   ext = response[:thumbnail][:extension]
+   fname = response[:thumbnail][:path]
    {
      name: response[:name],
      description: response[:description],
      image: "#{response[:thumbnail][:path]}.#{response[:thumbnail][:extension]}",
-     image_portrait: "#{response[:thumbnail][:path]}/portrait_xlarge.#{response[:thumbnail][:extension]}",
-     image_standard: "#{response[:thumbnail][:path]}/standard_amazing.#{response[:thumbnail][:extension]}",
-     image_landscape: "#{response[:thumbnail][:path]}/landscape_xlarge.#{response[:thumbnail][:extension]}"
+     images: {
+       standard: {
+         small: "#{fname}/standard_small.#{ext}",
+         medium: "#{fname}/standard_medium.#{ext}",
+         large: "#{fname}/standard_xlarge.#{ext}",
+         fantastic: "#{fname}/standard_fantastic.#{ext}",
+         amazing: "#{fname}/standard_amazing.#{ext}"
+       },
+       portrait: {
+         small: "#{fname}/portrait_small.#{ext}",
+         medium: "#{fname}/portrait_medium.#{ext}",
+         large: "#{fname}/portrait_xlarge.#{ext}",
+         fantastic: "#{fname}/portrait_fantastic.#{ext}",
+         uncanny: "#{fname}/portrait_uncanny.#{ext}",
+         amazing: "#{fname}/portrait_amazing.#{ext}"
+       },
+       landscape: {
+         small: "#{fname}/landscape_small.#{ext}",
+         medium: "#{fname}/landscape_medium.#{ext}",
+         large: "#{fname}/landscape_xlarge.#{ext}",
+         amazing: "#{fname}/landscape_amazing.#{ext}",
+         incredible: "#{fname}/landscape_incredible.#{ext}"
+       }
+     }
    }
  end
  def as_series
    character_series.map do |series|
+     fname = series[:thumbnail][:path]
+     ext = series[:thumbnail][:extension]
      {
        title: series[:title],
        description: series[:description],
-       image: "#{series[:thumbnail][:path]}/standard_small.#{series[:thumbnail][:extension]}"
+       image: "#{fname}/standard_small.#{ext}",
+       standard_images: {
+         small: "#{fname}/standard_small.#{ext}",
+         medium: "#{fname}/standard_medium.#{ext}",
+         large: "#{fname}/standard_large.#{ext}",
+         amazing: "#{fname}/standard_amazing.#{ext}"
+       },
+       portrait_images: {
+         small: "#{fname}/portrait_small.#{ext}",
+         medium: "#{fname}/portrait_medium.#{ext}",
+         large: "#{fname}/portrait_xlarge.#{ext}",
+         amazing: "#{fname}/portrait_amazing.#{ext}"
+       },
+       landscape_images: {
+         small: "#{fname}/landscape_small.#{ext}",
+         medium: "#{fname}/landscape_medium.#{ext}",
+         large: "#{fname}/landscape_large.#{ext}",
+         amazing: "#{fname}/landscape_amazing.#{ext}"
+       }
      }
    end
  end
